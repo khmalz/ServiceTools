1b9d3b65e1999cc28d82faf18a709927c8566135
khmalz <akmalrma1710@gmail.com>
2023-10-05 09:23:14 +0700

    Update content sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -25,6 +25,34 @@
                  </li>
                  <li>
                      <a href="{{ url('forms-elements.html') }}">
+                         <i class="bi bi-circle"></i><span>Waiting</span>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="{{ url('forms-elements.html') }}">
+                         <i class="bi bi-circle"></i><span>Pending</span>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="{{ url('forms-elements.html') }}">
+                         <i class="bi bi-circle"></i><span>Progress</span>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="{{ url('forms-elements.html') }}">
+                         <i class="bi bi-circle"></i><span>Done</span>
+                     </a>
+                 </li>
+             </ul>
+         </li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" data-bs-target="#appointment-nav" data-bs-toggle="collapse"
+                 href="{{ url('#') }}">
+                 <i class='bx bx-calendar-edit'></i><span>Appointment</span><i class="bi bi-chevron-down ms-auto"></i>
+             </a>
+             <ul id="appointment-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                 <li>
+                     <a href="{{ url('forms-elements.html') }}">
                          <i class="bi bi-circle"></i><span>Pending</span>
                      </a>
                  </li>
