f9029557b12b18704ab99ee54b89e87a9b220f6b
khmalz <akmalrma1710@gmail.com>
2023-10-05 10:55:45 +0700

    Update dashboard content

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -38,19 +38,13 @@
 
     @include('dashboard.layouts.sidebar')
 
-    <main id="main" class="main">
+    <main id="main" class="main" style="min-height: 90vh">
 
         @yield('content')
 
     </main><!-- End #main -->
 
-    <!-- ======= Footer ======= -->
-    <footer id="footer" class="footer">
-        <div class="copyright">
-            &copy;Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-        </div>
-    </footer>
-    <!-- End Footer -->
+    @include('dashboard.layouts.footer')
 
     <a href="{{ url('#') }}" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', fn () => view('home'))->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
-})->name('dashboard');
+})->middleware('auth')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
