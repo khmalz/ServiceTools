e132874dc7d0f72715ffa58ffee7b3fc06f76acd
khmalz <akmalrma1710@gmail.com>
2023-10-05 09:39:52 +0700

    Add role and permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([PermissionSeeder::class]);
 
-        // \App\Models\User::factory(10)->create();
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10)
+        ]);
+        $admin->assignRole('admin');
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 1; $i <= 8; $i++) {
+            $technician = User::create([
+                'name' => "Technician $i",
+                'email' => "technician$i@gmail.com",
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10)
+            ]);
+            $technician->assignRole('technician');
+        }
     }
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,5 +16,53 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $clientPermissions = [
+            'service_access',
+            'service_create',
+            'appointment_access',
+            'appointment_create'
+        ];
+
+        $technisianPermissions = [
+            'service_access',
+            'service_edit',
+            'appointment_access',
+            'appointment_edit',
+            'activity_access',
+        ];
+
+        $adminPermissions = [
+            'technician_access',
+            'technician_manage',
+            'service_access',
+            'service_edit',
+            'appointment_access',
+            'appointment_edit',
+            'activity_access',
+        ];
+
+        $allPermission = array_unique(array_merge($clientPermissions, $technisianPermissions, $adminPermissions));
+
+        foreach ($allPermission as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+
+        $role = Role::create(['name' => 'admin']);
+        foreach ($adminPermissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
+
+        $role = Role::create(['name' => 'technician']);
+        foreach ($technisianPermissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
+
+        $role = Role::create(['name' => 'client']);
+        foreach ($clientPermissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
     }
 }
