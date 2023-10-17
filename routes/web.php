0f801616521d297cfce3aa19b7b7f7affa090b82
khmalz <akmalrma1710@gmail.com>
2023-10-17 11:25:40 +0700

    make route more shorter

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,7 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('role:client')->group(function () {
         Route::get('/service/list', [ServiceClientController::class, 'list'])->name('service.list');
-        Route::get('/service', [ServiceClientController::class, 'create'])->name('service.create');
-        Route::post('/service/create', [ServiceClientController::class, 'store'])->name('service.store');
-        Route::patch('/service/{service}', [ServiceClientController::class, 'update'])->name('service.update');
-        Route::get('/service/{service}', [ServiceClientController::class, 'show'])->name('service.show');
+        Route::resource('service', ServiceClientController::class)->except('index', 'destroy');
 
         Route::get('/appointment/list', [AppointmentClientController::class, 'list'])->name('appointment.list');
         Route::get('/appointment/{appointment}', [AppointmentClientController::class, 'show'])->name('appointment.show');
